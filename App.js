fc038a18d89d4e97c1d2002f5011ecc07142779b
Daniel Garcia <jdanielink@gmail.com>
2019-12-09 23:08:08 -0600

    Init commit

---
+++ App.js
@@ -0,0 +1,49 @@
+import React, { useState, useEffect, Fragment } from 'react';
+import { StyleSheet, View, TextInput, Button, Text, ScrollView } from 'react-native';
+
+export default function App() {
+  const [note, setNote] = useState('');
+  const [notes, setNotes] = useState([]);
+
+  const inputHandle = text => {
+    setNote(text);
+  };
+
+  const addNote = () => {
+    setNotes(notes => [...notes, note]);
+    setNote('');
+  };
+
+  const deleteNote = index => {
+    setNotes(notes => notes.slice(index, 1));
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.containerTwo}>
+        <TextInput placeholder="..." onChangeText={inputHandle} value={note} />
+        <Button title="Add" onPress={addNote} />
+      </View>
+      <ScrollView>
+        {notes.map((note, index) => (
+          <View key={index}>
+            <Text>{note}</Text>
+            <Button onPress={() => deleteNote(index)} title="Remove" />
+          </View>
+        ))}
+      </ScrollView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  containerTwo: {
+    flexDirection: 'row'
+  }
+});
